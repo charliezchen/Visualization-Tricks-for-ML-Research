636530a5be64f1b4cb0cdac185cee758c6958a4a
AndPotap <apotapczynski@gmail.com>
2024-10-07 10:43:15 -0400

    Added similar colors to the plot

--- scaling_laws/data_fns.py
+++ scaling_laws/data_fns.py
@@ -3,14 +3,29 @@ import numpy as np
 
 def get_scaling_law_data():
     data = []
-    cases = [0.25, 0.5, 1., 2., 4.]
-    start_stop_map = {0.25: (1, 20), 0.5: (3, 30), 1.: (6, 20), 2.: (10, 50), 4.: (10, 80)}
+    cases = [0.25, 0.5, 1., 2., 4., 16., 64., 256., 1024., 32_768.]
+    start_stop_map = {
+        0.25: (1, 20),
+        0.5: (3, 30),
+        1.: (6, 20),
+        2.: (10, 30),
+        4.: (10, 40),
+        16.: (15, 45),
+        64.: (20, 50),
+        256.: (25, 55),
+        1024.: (30, 57),
+        32_768.: (30, 60),
+    }
     for case in cases:
         start, stop = start_stop_map[case]
-        x = np.linspace(start, stop, num=5)
-        y = taylor(x, A=3, C=0, k=0.15, x0=x[2])
+        N = 5
+        x = np.linspace(start, stop, num=N)
+        y = taylor(x, A=3, C=0, k=0.15, x0=x[N // 2])
         data.append({"compute": x, "loss": y, "params": case})
-    return data
+
+        def fn(x):
+            return exponential_decay(x, A=3., k=0.15, C=0.)
+    return data, fn
 
 
 def taylor(x, A, C, k, x0):
--- scaling_laws/plot_scaling_law.py
+++ scaling_laws/plot_scaling_law.py
@@ -1,22 +1,40 @@
 import numpy as np
-from matplotlib import pyplot as plt
 import seaborn as sns
+from matplotlib import pyplot as plt
 
 from scaling_laws.data_fns import get_scaling_law_data
-from scaling_laws.data_fns import exponential_decay
 
-data_list = get_scaling_law_data()
-x = np.linspace(1, 100, num=50)
-y = exponential_decay(x, A=3., k=0.15, C=0.)
+data_list, fn = get_scaling_law_data()
+x = np.linspace(1, 60, num=50)
+y = fn(x)
 
-sns.set(style="whitegrid", font_scale=2.0, rc={"lines.linewidth": 3.0})
+# https://colorbrewer2.org/#type=diverging&scheme=RdBu&n=6
+plot_map = {
+    0.25: {"color": "#2166ac", "label": "B1", "params": "250K"},
+    0.5: {"color": "#2166ac", "label": None, "params": "500K"},
+    1.: {"color": "#2166ac", "label": None, "params": "1M"},
+    2.: {"color": "#67a9cf", "label": "B2", "params": "2M"},
+    4.: {"color": "#fddbc7", "label": "B3", "params": "4M"},
+    16.: {"color": "#fddbc7", "label": None, "params": "16M"},
+    64.: {"color": "#fddbc7", "label": None, "params": "64M"},
+    256.: {"color": "#ef8a62", "label": "B4", "params": "256M"},
+    1024.: {"color": "#ef8a62", "label": None, "params": "1024M"},
+    32_768.: {"color": "#b2182b", "label": "B5", "params": "32B"},
+}
+
+sns.set(style="whitegrid", font_scale=2.0)
 plt.figure(dpi=100, figsize=(10, 8))
 for data in data_list:
-    plt.plot(data["compute"], data["loss"])
-    plt.scatter(data["compute"], data["loss"])
-plt.plot(x, y, color="black")
+    params = data["params"]
+    color = plot_map[params]["color"]
+    label = plot_map[params]["label"]
+    plt.plot(data["compute"], data["loss"], lw=2, alpha=0.5, c=color)
+    plt.scatter(data["compute"], data["loss"], lw=3, c=color, label=label, marker="o")
+plt.plot(x, y, color="black", linestyle="--", lw=3)
+plt.text(2, 0.002, r"$3 e^{-0.15 C}$", fontsize=25, color="black")
 plt.yscale("log")
 plt.ylabel("Loss")
 plt.xlabel("Compute (PFLOPs)")
+plt.legend()
 plt.tight_layout()
 plt.show()
